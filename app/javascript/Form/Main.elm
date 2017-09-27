404d53010c91a2db814f3006ffd39342c2890f73
Jan Klimo <jan.klimo@gmail.com>
2017-09-27 15:09:49 +0700

    finished success screen

--- app/javascript/Form/Main.elm
+++ app/javascript/Form/Main.elm
@@ -1,6 +1,6 @@
 module Form.Main exposing (..)
 
-import Html exposing (Html, program, input, div, form, label, text, a)
+import Html exposing (Html, program, input, div, img, form, label, text, a, h1, p)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import RemoteData exposing (RemoteData(..))
@@ -48,6 +48,22 @@ update msg model =
             , createCallMsg model
             )
 
+        BackToFormWithoutReset ->
+            { model
+                | requestStatus = NotAsked
+                , validating = False
+            }
+                ! []
+
+        BackToFormResetRecipient ->
+            { model
+                | requestStatus = NotAsked
+                , recipientName = ""
+                , recipientEmail = ""
+                , validating = False
+            }
+                ! []
+
         HandleCreateCallResponse data ->
             { model | requestStatus = data } ! []
 
@@ -75,6 +91,7 @@ callForm model =
                 [ label [ for "senderName", class "col-form-label" ] [ text "Your Name" ]
                 , input
                     [ type_ "text"
+                    , value model.senderName
                     , id "senderName"
                     , class ("form-control " ++ validate model.senderName model.validating)
                     , placeholder "Your Name"
@@ -86,6 +103,7 @@ callForm model =
                 [ label [ for "senderEmail", class "col-form-label" ] [ text "Your Email" ]
                 , input
                     [ type_ "email"
+                    , value model.senderEmail
                     , id "senderEmail"
                     , class ("form-control " ++ validate model.senderEmail model.validating)
                     , placeholder "Sender Email"
@@ -99,6 +117,7 @@ callForm model =
                 [ label [ for "recipientName", class "col-form-label" ] [ text "Recipient's Name" ]
                 , input
                     [ type_ "text"
+                    , value model.recipientName
                     , id "recipientName"
                     , class ("form-control " ++ validate model.recipientName model.validating)
                     , placeholder "Recipient's Name"
@@ -110,6 +129,7 @@ callForm model =
                 [ label [ for "recipientEmail", class "col-form-label" ] [ text "Recipient's Email" ]
                 , input
                     [ type_ "email"
+                    , value model.recipientEmail
                     , id "recipientEmail"
                     , class ("form-control " ++ validate model.recipientEmail model.validating)
                     , placeholder "Recipient's Email"
@@ -125,8 +145,8 @@ type alias Disabled =
     Bool
 
 
-submitButton : Model -> Disabled -> Html Msg
-submitButton model disabled =
+submitButton : Model -> Msg -> Disabled -> Html Msg
+submitButton model msg disabled =
     let
         css =
             if disabled then
@@ -149,7 +169,7 @@ submitButton model disabled =
         div [ class "text-center mt-3" ]
             [ a
                 [ class ("btn btn-primary form-submit " ++ css)
-                , onClick (submitIfValid model)
+                , onClick msg
                 , href "#"
                 ]
                 [ children ]
@@ -170,21 +190,45 @@ view model =
     case model.requestStatus of
         NotAsked ->
             div []
-                [ callForm model
-                , submitButton model False
+                [ h1 [ class "text-center" ] [ text ("Let's Do It!") ]
+                , callForm model
+                , submitButton model (submitIfValid model) False
                 ]
 
         Loading ->
             div []
-                [ callForm model
-                , submitButton model True
+                [ h1 [ class "text-center" ] [ text ("Let's Do It!") ]
+                , callForm model
+                , submitButton model (submitIfValid model) True
                 ]
 
         Failure a ->
-            div [] [ text "oh noez" ]
+            div []
+                [ h1 [ class "text-center" ] [ text ("Oh Noez") ]
+                , p [ class "text-center mb-4" ] [ text "Something went horribly wrong. Try again?" ]
+                , submitButton model BackToFormWithoutReset False
+                ]
 
         Success a ->
-            div [] [ text "yay" ]
+            div [ class "text-center" ]
+                [ h1 [] [ text ("Yeehaaa!") ]
+                , img
+                    [ src "https://s3-us-west-2.amazonaws.com/callforgiftideas/aw_yiss.png"
+                    , class "aw-yiss"
+                    ]
+                    []
+                , p []
+                    [ text
+                        ("Thank you, "
+                            ++ model.senderName
+                            ++ "! We've sent an email to "
+                            ++ model.recipientName
+                            ++ ". You'll receive a notification once they submit their wishlist!"
+                        )
+                    ]
+                , p [ class "mb-4" ] [ text "Ready to send another one?" ]
+                , submitButton model BackToFormResetRecipient False
+                ]
 
 
 valid : Model -> Bool
--- app/javascript/Form/Msgs.elm
+++ app/javascript/Form/Msgs.elm
@@ -11,3 +11,5 @@ type Msg
     | Submit
     | Validate
     | HandleCreateCallResponse (WebData String)
+    | BackToFormResetRecipient
+    | BackToFormWithoutReset
