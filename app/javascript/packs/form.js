832d32fa8c5c42059df7ef582162a7b43ec49350
Jan Klimo <jan.klimo@gmail.com>
2017-11-02 00:50:56 +0700

    dumb preloading of form assets

--- app/javascript/packs/form.js
+++ app/javascript/packs/form.js
@@ -6,14 +6,3 @@ document.addEventListener('DOMContentLoaded', () => {
     Elm.Form.Main.embed(node)
   }
 })
-
-const preload = [
-  "https://s3-us-west-2.amazonaws.com/callforgiftideas/aw_yiss.png",
-  "https://s3-us-west-2.amazonaws.com/callforgiftideas/ripple.gif"
-];
-
-let images = [];
-for (let i = 0; i < preload.length; i++) {
-    images[i] = new Image();
-    images[i].src = preload[i];
-}
